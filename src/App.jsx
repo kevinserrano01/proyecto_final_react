6c445183a127846a70cac8ee20febe02b3bc437f
Kev <kevinserrano010@gmail.com>
2024-07-12 17:07:19 -0300

    Creacion de rutas y el componente NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
+import { Navigate, Route, Routes } from "react-router-dom"
+import { NavBar } from "./components/NavBar"
+import { HomeScreen } from "./routes/HomeScreen"
+import { Albums } from './routes/Albums'
+import { Artist } from './routes/Artist'
+import { Explore } from './routes/Explore'
+import { Favorites } from './routes/Favorites'
+import { Genres } from './routes/Genres'
+import { Music } from './routes/Music'
+import { PlayList } from './routes/PlayList'
+import { Podcast } from './routes/Podcast'
+
+
+
 export const App = () => {
   return (
     <>
+      <NavBar></NavBar>
       <h1>Kev</h1>
+
+      <Routes>
+          <Route path="/" element={ <HomeScreen></HomeScreen> }></Route>
+          <Route path="/Albums" element={ <Albums></Albums> }></Route>
+          <Route path="/Artist" element={ <Artist></Artist> }></Route>
+          <Route path="/Explore" element={ <Explore></Explore> }></Route>
+          <Route path="/Favorites" element={ <Favorites></Favorites> }></Route>
+          <Route path="/Genres" element={ <Genres></Genres> }></Route>
+          <Route path="/Music" element={ <Music></Music> }></Route>
+          <Route path="/PlayList" element={ <PlayList></PlayList> }></Route>
+          <Route path="/Podcast" element={ <Podcast></Podcast> }></Route>
+          {/* Ruta por defecto si ponemos cualquier cosa despues de la barra nos lleva al home. */}
+          <Route path="/*" element={<Navigate to="/" />}></Route>
+        </Routes>
     </>
   )
 }
---
+++ src/routes/Podcast.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Podcast = () => {
+  return (
+    <div>Podcast</div>
+  )
+}
