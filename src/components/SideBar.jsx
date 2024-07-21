b1dc09c35c37da75f69ed536d3451a3dcb196725
Kev <kevinserrano010@gmail.com>
2024-07-21 00:19:13 -0300

    Actualizacion en SideBar - Cambio de lugares

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -6,11 +6,11 @@ export const SideBar = () => {
   return (
     <div className="sidebar d-flex flex-column p-3">
         <NavLink to='/explore' className="mb-2">Explore</NavLink>
-        <NavLink to='/albums' className="mb-2" > Albums </NavLink>
+        <NavLink to='/favorites' className="mb-2" > Favorites </NavLink>
         <NavLink to='/playlist' className="mb-2" > PlayLists </NavLink>
+        <NavLink to='/albums' className="mb-2" > Albums </NavLink>
         <NavLink to='/genres' className="mb-2" > Genres </NavLink>
         <NavLink to='/artist' className="mb-2" > Artist </NavLink>
-        <NavLink to='/favorites' className="mb-2" > Favorites </NavLink>
     </div>
   )
 }
