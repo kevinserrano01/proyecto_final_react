28847b3107966736e6213f136e9fbe693303e4bc
Kev <kevinserrano010@gmail.com>
2024-07-31 10:07:08 -0300

    Creacion de Componente SongForm [Agregar y editar Cancion

---
+++ src/components/SongForm.jsx
@@ -0,0 +1,49 @@
+
+export const SongForm = () => {
+  return (
+    <section className="section">
+            <div className="columns is-centered">
+                <div className="column is-4">
+                    <form>
+                        <div className="field">
+                            <label htmlFor="username">Song name:</label>
+                            <div className="control has-icons-left">
+                                <input
+                                    className="input"
+                                    type="text"
+                                    id="name"
+                                    name="name"
+                                    defaultValue=""
+                                    // onChange={handleChange}
+                                />
+                            </div>
+                        </div>
+                        <div className="field">
+                            <label htmlFor="author">Author</label>
+                            <div className="control has-icons-left">
+                                <input
+                                    className="input"
+                                    type="text"
+                                    id="author"
+                                    name="author"
+                                    defaultValue=""
+                                    // onChange={handleChange}
+                                />
+                            </div>
+                        </div>
+                        <div className="field">
+                            <div className="control">
+                                <button
+                                    type="submit"
+                                    className="button is-primary is-fullwidth"
+                                >
+                                    Send
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </section>
+  )
+}
