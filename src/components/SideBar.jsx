04fb362aeb394c67b79c9b3b7ac684c941e049ff
Kev <kevinserrano010@gmail.com>
2024-07-31 15:06:22 -0300

    Actualizacion en SideBar, uso NavBar para el estado del link actual con estilos.

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,17 +1,100 @@
 import React from 'react'
 import '../styles/SideBar.css'
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 export const SideBar = () => {
   return (
     <div className="sidebar d-flex flex-column p-3">
-        <NavLink to='/search' className="mb-2" > Search </NavLink>
-        <NavLink to='/explore' className="mb-2">Explore</NavLink>
-        <NavLink to='/favorites' className="mb-2" > Favorites </NavLink>
-        <NavLink to='/playlist' className="mb-2" > PlayLists </NavLink>
-        <NavLink to='/albums' className="mb-2" > Albums </NavLink>
-        <NavLink to='/genres' className="mb-2" > Genres </NavLink>
-        <NavLink to='/artist' className="mb-2" > Artist </NavLink>
+      <NavLink
+        to="/search"
+        className={({ isActive, isPending, isTransitioning }) =>
+          [
+            isPending ? "pending" : "",
+            isActive ? "text-white" : "",
+            isTransitioning ? "transitioning" : "",
+          ].join(" ")
+        }
+      >
+        Search
+      </NavLink>
+
+      <NavLink
+        to="/explore"
+        className={({ isActive, isPending, isTransitioning }) =>
+          [
+            isPending ? "pending" : "",
+            isActive ? "text-white" : "",
+            isTransitioning ? "transitioning" : "",
+          ].join(" ")
+        }
+      >
+        Explore
+      </NavLink>
+
+      <NavLink
+        to="/favorites"
+        className={({ isActive, isPending, isTransitioning }) =>
+          [
+            isPending ? "pending" : "",
+            isActive ? "text-white" : "",
+            isTransitioning ? "transitioning" : "",
+          ].join(" ")
+        }
+      >
+        Favorites
+      </NavLink>
+
+      <NavLink
+        to="/playlist"
+        className={({ isActive, isPending, isTransitioning }) =>
+          [
+            isPending ? "pending" : "",
+            isActive ? "text-white" : "",
+            isTransitioning ? "transitioning" : "",
+          ].join(" ")
+        }
+      >
+        PlayLists
+      </NavLink>
+
+      <NavLink
+        to="/albums"
+        className={({ isActive, isPending, isTransitioning }) =>
+          [
+            isPending ? "pending" : "",
+            isActive ? "text-white" : "",
+            isTransitioning ? "transitioning" : "",
+          ].join(" ")
+        }
+      >
+        Albums
+      </NavLink>
+
+      <NavLink
+        to="/genres"
+        className={({ isActive, isPending, isTransitioning }) =>
+          [
+            isPending ? "pending" : "",
+            isActive ? "text-white" : "",
+            isTransitioning ? "transitioning" : "",
+          ].join(" ")
+        }
+      >
+        Genres
+      </NavLink>
+
+      <NavLink
+        to="/artist"
+        className={({ isActive, isPending, isTransitioning }) =>
+          [
+            isPending ? "pending" : "",
+            isActive ? "text-white" : "",
+            isTransitioning ? "transitioning" : "",
+          ].join(" ")
+        }
+      >
+        Artist
+      </NavLink>
     </div>
-  )
+  );
 }
