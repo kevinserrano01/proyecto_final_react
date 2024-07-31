9aed5b26957c2a0d32a42a8cc182712f85350ada
Kev <kevinserrano010@gmail.com>
2024-07-31 15:08:48 -0300

    Actualizacion en NavBar - Uso Link en vez de NavLink

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,11 @@
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 export const NavBar = () => {
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-dark fixed-top">
         <div className="container-fluid">
-            <NavLink to='/' className="navbar-brand"> App Music </NavLink>
+            <Link to='/' className="navbar-brand"> App Music </Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
@@ -19,7 +19,7 @@ export const NavBar = () => {
                     </button>
                     </li>
                     <li className="nav-item">
-                      <NavLink to='/login' className="nav-link" > Logout </NavLink>
+                      <Link to='/login' className="nav-link" > Logout </Link>
                     </li>
                 </ul>
             </div>
